31222180e28cdd627cfbf7c472a6490588f7fc97
kxdan <daniel--morris@outlook.com>
2023-03-15 18:00:17 -0700

    left and down shift work now, added asserts

--- 2048Game.py
+++ 2048Game.py
@@ -1,3 +1,4 @@
+import copy
 moves = "D R D L U"
 
 
@@ -17,7 +18,20 @@ def main(direction, grid):
         parsedMap.append(intRow)
 
     
-    move("d", parsedMap)
+    mapToDownshift = parsedMap.copy()
+    mapToMoveLeft = copy.deepcopy(parsedMap)
+
+    downshiftGrid = [[0,0,0,0],[2,2,0,2],[4,4,4,4],[4,4,4,4]]
+
+    resultingGridDown = moveDown(mapToDownshift)
+
+    assert(resultingGridDown == downshiftGrid)
+
+    leftShiftGrid = [[4,4,2,0],[4,4,4,0],[4,4,0,0],[2,4,2,0]]
+
+    resultingGridLeft = moveLeft(mapToMoveLeft)
+
+    assert(resultingGridLeft == leftShiftGrid)
 
     print("x") 
 
@@ -27,16 +41,82 @@ board = """2 2 4 2
 2 4 0 2
 """
 
+boardMovedLeft = """4 4 2 0
+4 4 4 0
+4 4 0 0
+2 4 2 0
+"""
 
-boardWithEdits = """0 0 0 0
+boardMovedDown = """0 0 0 0
 2 2 0 2
 4 4 4 4
 4 4 4 4
 """
 
-def move(direction, grid):
+def moveLeft(grid):
+    gridlen = len(grid)
 
-    print(grid)
+    previouslyCombined = [[False]*gridlen for _ in range(gridlen)]
+
+    #Iterate through rows
+    for row in range(gridlen):
+        #Iterate through columns, do combination
+        prevValue = grid[row][0]
+        for column in range(1, gridlen):
+            currentValue = grid[row][column]
+        
+            if (column+1 < gridlen):
+                nextVal = grid[row][column+1]
+                if(currentValue == prevValue and currentValue == nextVal):
+
+                    allEqual = True
+                    firstElement = grid[row]
+                    for element in grid:
+                        if element != firstElement:
+                            allEqual == False
+
+                    #check they're not all equal
+                    if not allEqual:
+                        continue
+
+            if prevValue == currentValue and not previouslyCombined[row][column-1]:
+                previouslyCombined[row][column] = True 
+                currentValue=prevValue+currentValue 
+                grid[row][column] = currentValue
+                grid[row][column-1] = 0
+            else:
+                grid[row][column] = currentValue
+            
+            if currentValue == 0:
+                currentValue = prevValue
+                prevValue = 0
+            
+            prevValue = currentValue
+
+        prevValue = grid[row][0]
+
+        #Remove 0's in new row
+        arrayToPop = []
+        for column in range(0, gridlen):
+            arrayToPop.append(grid[row][column])     
+
+        countOfZeros = 0
+
+        filteredArray = []
+        for element in arrayToPop:
+            if element != 0:
+                filteredArray.append(element)
+            else:
+                countOfZeros+=1
+        
+        for j in range(0, countOfZeros):
+            filteredArray.append(0)
+
+        grid[row] = filteredArray
+
+    return grid
+
+def moveDown(grid):
 
     gridlen = len(grid)
 
@@ -88,7 +168,6 @@ def move(direction, grid):
         for x in range(0, len(filteredArray)):
             grid[x][j] = filteredArray[x]
 
-    print(grid)
-
+    return grid
 
 main("d", board)
