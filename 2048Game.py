c6074113cd121c466da10e3d6073f723fc6f0bbd
kxdan <daniel--morris@outlook.com>
2023-03-15 17:52:06 -0700

    Revert "working left shift too"
    This reverts commit 0ab683bc6684b700d8cf1fcc99d5620303f56ba4.

--- 2048Game.py
+++ 2048Game.py
@@ -17,9 +17,7 @@ def main(direction, grid):
         parsedMap.append(intRow)
 
     
-    moveLeft(parsedMap)
-
-    moveDown(parsedMap)
+    move("d", parsedMap)
 
     print("x") 
 
@@ -29,85 +27,14 @@ board = """2 2 4 2
 2 4 0 2
 """
 
-boardMovedLeft = """4 4 2 0
-4 4 4 0
-4 4 0 0
-2 4 2 0
-"""
 
-boardMovedDown = """0 0 0 0
+boardWithEdits = """0 0 0 0
 2 2 0 2
 4 4 4 4
 4 4 4 4
 """
 
-def moveLeft(grid):
-    print(grid)
-
-    gridlen = len(grid)
-
-    previouslyCombined = [[False]*gridlen for _ in range(gridlen)]
-
-    #Iterate through rows
-    for row in range(gridlen):
-        #Iterate through columns, do combination
-        prevValue = grid[row][0]
-        for column in range(1, gridlen):
-            currentValue = grid[row][column]
-        
-            if (column+1 < gridlen):
-                nextVal = grid[row][column+1]
-                if(currentValue == prevValue and currentValue == nextVal):
-
-                    allEqual = True
-                    firstElement = grid[row]
-                    for element in grid:
-                        if element != firstElement:
-                            allEqual == False
-
-                    #check they're not all equal
-                    if not allEqual:
-                        continue
-
-            if prevValue == currentValue and not previouslyCombined[row][column-1]:
-                previouslyCombined[row][column] = True 
-                currentValue=prevValue+currentValue 
-                grid[row][column] = currentValue
-                grid[row][column-1] = 0
-            else:
-                grid[row][column] = currentValue
-            
-            if currentValue == 0:
-                currentValue = prevValue
-                prevValue = 0
-            
-            prevValue = currentValue
-
-        prevValue = grid[row][0]
-
-        #Remove 0's in new row
-        arrayToPop = []
-        for column in range(0, gridlen):
-            arrayToPop.append(grid[row][column])     
-
-        countOfZeros = 0
-
-        filteredArray = []
-        for element in arrayToPop:
-            if element != 0:
-                filteredArray.append(element)
-            else:
-                countOfZeros+=1
-        
-        for j in range(0, countOfZeros):
-            filteredArray.append(0)
-
-        grid[row] = filteredArray
-
-    print(grid)
-
-
-def moveDown(grid):
+def move(direction, grid):
 
     print(grid)
 
@@ -115,10 +42,8 @@ def moveDown(grid):
 
     previouslyCombined = [[False]*gridlen for _ in range(gridlen)]
 
-    #Iterate through columns
     for j in range(len(grid[0])):
 
-        #Iterate through rows, do combination
         prevValue = grid[0][j]
         for x in range(1, gridlen):
             currentValue = grid[x][j]
@@ -143,9 +68,8 @@ def moveDown(grid):
             prevValue = currentValue
 
         prevValue = grid[0][j]
-
-        #Remove 0's in new row
         arrayToPop = []
+
         for x in range(0, gridlen):
             arrayToPop.append(grid[x][j])     
 
